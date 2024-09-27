c0d97027d1262d1561afa7d011052a6de8a74cd8
Kelsey Mersinas <kelsey.merisnas@gmail.com>
2024-09-26 20:39:14 -0400

    updating app folder

--- app/app.py
+++ app/app.py
@@ -5,22 +5,30 @@ import pickle
 
 app = Flask(__name__)
 
-# load the model
-with open('mushroom_model.h5', 'rb') as file:
+# Load the model
+with open('mushroom_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Attempt to make the prediction page - havn't figured it out yet just slapped this together- will work on over weekend
 @app.route('/predict', methods=['POST'])
 def predict():
-    features_input = request.form['features']  
-    features = np.array(features_input.split(',')).reshape(1, -1)  
-    prediction = model.predict(features)
-    return jsonify({'edible': prediction[0]})
+    content = request.json['data']
+    print(content)
+    return(jsonify({'ok': True}))
 
 # home page
 @app.route('/')
 def home():
     return render_template('landing.html')
+
+# dashboard 1
+@app.route('/tableauOne')
+def tableauOne():
+    return render_template('tableauOne.html')
+
+# dashboard 2
+@app.route('/tableauTwo')
+def tableauTwo():
+    return render_template('tableauTwo.html')
 
 # write up
 @app.route('/writeup')
@@ -36,6 +44,20 @@ def aboutus():
 @app.route('/workscited', methods=['GET'])
 def workscited():
     return render_template('workscited.html')
+
+##################################################
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
 
 if __name__ == '__main__':
     app.run(debug=True)
